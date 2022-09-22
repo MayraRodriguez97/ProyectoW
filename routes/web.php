c8843913cfc9b37ae57366ea0392a5a4963694f6
MayraRodriguez97 <rodriguezmayra739@gmail.com>
2022-09-21 18:23:59 -0600

    plantilla

--- routes/web.php
+++ routes/web.php
@@ -14,5 +14,8 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return view('welcome');
+    return view('index');
+});
+Route::get('/plantilla2', function () {
+    return view('plantilla2');
 });
